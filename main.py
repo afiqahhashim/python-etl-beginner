d054a9eb5e59bac536985ac1f6a139389086c286
afiqahhashim <80879125+afiqahhashim@users.noreply.github.com>
2026-09-16 21:55:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-"""
-Python Extract Transform Load Example
-"""
 import requests
 import pandas as pd
 from sqlalchemy import (create_engine)
@@ -22,17 +19,17 @@ def transform(data:dict) -> pd.DataFrame:
     print(f"Total number of universities from API: {len(data)}")
     df = df[df['name'].str.contains("California")]
     print(f"Total number of universities in California: {len(df)}")
-    df['domains'] = [','.join(map(str, l)) for l in df['domains']]
+    df['domains'] = [','.join(map(str, l)) for l in df['domains']] #split into comma separated string
     df['web_pages'] = [','.join(map(str, l)) for l in df['web_pages']]
     df = df.reset_index(drop=True)
     return df[['domains', 'web_pages', 'country', 'name']]
 
+# funstion to load data to database
 def load(df:pd.DataFrame)-> None:
-    """ Loads data into a sqllite database"""
     disk_engine = create_engine('sqlite:///my_lite_store.db')
     df.to_sql('cal_uni', disk_engine, if_exists='replace')
 
-# %%
+
 data = extract()
 df = transform(data)
 load(df)
